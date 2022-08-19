5f6635f589ff912a06c7359e23fd54f8ad90ee2c
bimri <bimri@outlook.com>
2022-08-20 02:20:07 +0300

    chap02: <console> classes

--- chap02/console/bin/main.dart
+++ chap02/console/bin/main.dart
@@ -2,10 +2,12 @@ import 'package:console/01-declaring_variables.dart';
 import 'package:console/02-strings.dart';
 import 'package:console/03-functions.dart';
 import 'package:console/04-classes.dart';
+import 'package:console/05-generics.dart';
 
 main() {
   variablePlayground();
   stringPlayground();
   functionPlayground();
   classPlayground();
+  genericsPlayground();
 }
