79024718c24d108682092b0a29691588efabbbe6
bimri <bimri@outlook.com>
2022-08-30 19:06:50 +0300

    Catching common JSON errors

--- chap08/lib/pizza.dart
+++ chap08/lib/pizza.dart
@@ -1,3 +1,11 @@
+// we simply used constants instead of repeating the name
+// of the keys for our JSON data.
+const keyId = 'id';
+const keyName = 'pizzaName';
+const keyDescription = 'description';
+const keyPrice = 'price';
+const keyImage = 'imageUrl';
+
 class Pizza {
   int? id;
   String? pizzaName;
@@ -10,45 +18,40 @@ class Pizza {
   // factory constructor method
   factory Pizza.fromJsonOrNull(Map<String, dynamic> json) {
     Pizza pizza = Pizza();
-    pizza.id = (json['id'] != null) ? int.tryParse(json['id'].toString()) : 0;
-    pizza.pizzaName =
-        (json['pizzaName'] != null) ? json['pizzaName'].toString() : '';
+    pizza.id = (json[keyId] != null) ? int.tryParse(json[keyId].toString()) : 0;
+    pizza.pizzaName = (json[keyName] != null) ? json[keyName].toString() : '';
     pizza.description =
-        (json['description'] != null) ? json['description'].toString() : '';
-    pizza.price = (json['price'] != null &&
-            double.tryParse(json['price'].toString()) != null)
-        ? json['price']
+        (json[keyDescription] != null) ? json[keyDescription].toString() : '';
+    pizza.price = (json[keyPrice] != null &&
+            double.tryParse(json[keyPrice].toString()) != null)
+        ? json[keyPrice]
         : 0.0;
-    pizza.imageUrl =
-        (json['imageUrl'] != null) ? json['imageUrl'].toString() : '';
-
-    if (pizza.id == 0 || pizza.pizzaName?.trim() == '') {
-      return null;
-    }
+    pizza.imageUrl = (json[keyImage] != null) ? json[keyImage].toString() : '';
+    if (pizza.id == 0 || pizza.pizzaName?.trim() == '') {}
     return pizza;
   }
 
   // Transforming the Map objects into Pizza objects
   Pizza.fromJson(Map<String, dynamic> json) {
-    id = (json['id'] != null) ? int.tryParse(json['id'].toString()) : 0;
-    pizzaName = (json['pizzaName'] != null) ? json['pizzaName'].toString() : '';
+    id = (json[keyId] != null) ? int.tryParse(json['id'].toString()) : 0;
+    pizzaName = (json[keyName] != null) ? json[keyName].toString() : '';
     description =
-        (json['description'] != null) ? json['description'].toString() : '';
-    price = (json['price'] != null &&
-            double.tryParse(json['price'].toString()) != null)
-        ? json['price']
+        (json[keyDescription] != null) ? json[keyDescription].toString() : '';
+    price = (json[keyPrice] != null &&
+            double.tryParse(json[keyPrice].toString()) != null)
+        ? json[keyPrice]
         : 0.0;
-    imageUrl = (json['imageUrl'] != null) ? json['imageUrl'].toString() : '';
+    imageUrl = (json[keyImage] != null) ? json[keyImage].toString() : '';
   }
 
   // transform a Dart class into a JSON string
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'pizzaName': pizzaName,
-      'description': description,
-      'price': price,
-      'imageUrl': imageUrl,
+      keyId: id,
+      keyName: pizzaName,
+      keyDescription: description,
+      keyPrice: price,
+      keyImage: imageUrl,
     };
   }
 }
