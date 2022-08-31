ae6bac03dd45f27d666111a9a9e467d9419a87c3
bimri <bimri@outlook.com>
2022-09-01 00:02:13 +0300

    HttpHelper class

--- chap08/lib/httphelper.dart
+++ chap08/lib/httphelper.dart
@@ -1,9 +1,17 @@
-import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'pizza.dart';
 
 class HttpHelper {
+  // first time you call the factory constructor, it will return
+  // a new instance of HttpHelper. Once HttpHelper has been instantiated, the constructor
+  // will not build a new instance of HttpHelper; it will only return the existing one.
+  static final HttpHelper _httpHelper = HttpHelper._internal();
+  HttpHelper._internal();
+  factory HttpHelper() {
+    return _httpHelper;
+  }
+
   final String authority = '02z2g.mocklab.io';
   final String path = 'pizzalist';
 
