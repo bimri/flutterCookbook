f0d21795dd2216a51a1c44dd2d004d0f082a46a8
bimri <bimri@outlook.com>
2022-09-02 03:20:04 +0300

    Allowing multiple stream subscriptions

--- chap09/lib/main.dart
+++ chap09/lib/main.dart
@@ -43,6 +43,8 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  StreamSubscription? subscription2;
+  String values = '';
   StreamSubscription? subscription;
   StreamTransformer? transformer;
   int? lastNumber;
@@ -65,7 +67,7 @@ class _StreamHomePageState extends State<StreamHomePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(lastNumber.toString()),
+            Text(values),
             ElevatedButton(
               onPressed: () => addRandomNumber(),
               child: const Text('New Random Number'),
@@ -109,11 +111,16 @@ stream when it has completed its tasks */
   void initState() {
     numberStream = NumberStream();
     numberStreamController = numberStream?.controller;
-    Stream? stream = numberStreamController?.stream;
+    Stream? stream = numberStreamController?.stream.asBroadcastStream();
 
     subscription = stream?.listen((event) {
       setState(() {
-        lastNumber = event;
+        values += '$event - ';
+      });
+    });
+    subscription2 = stream?.listen((event) {
+      setState(() {
+        values += '$event - ';
       });
     });
     super.initState();
