e4de3f6128155b2afd2b81d01e139beb0ea02e72
bimri <bimri@outlook.com>
2022-09-04 19:34:13 +0300

    Creating your own package (part 2)

---
+++ chap10/plugins/packages/area/lib/triangle.dart
@@ -0,0 +1,8 @@
+part of area;
+
+String calculateAreaTriangle(double width, double height) {
+  double result = width * height / 2;
+  final formatter = NumberFormat('#.####');
+  return formatter.format(result);
+}
+ 
