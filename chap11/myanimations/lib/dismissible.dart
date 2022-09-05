cc467465c4fc0040407f0608238a595dfff38d18
bimri <bimri@outlook.com>
2022-09-05 03:41:32 +0300

    Using the animations Flutter package

--- chap11/myanimations/lib/dismissible.dart
+++ chap11/myanimations/lib/dismissible.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:animations/animations.dart';
 
 class DismissibleScreen extends StatefulWidget {
   const DismissibleScreen({super.key});
@@ -23,23 +24,54 @@ class _DismissibleScreenState extends State<DismissibleScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dismissible Example'),
-      ),
-      body: ListView.builder(
-        itemCount: sweets.length,
-        itemBuilder: (context, index) {
-          return Dismissible(
-            key: Key(sweets[index]),
-            child: ListTile(
-              title: Text(sweets[index]),
-            ),
-            onDismissed: (direction) {
-              sweets.removeAt(index);
-            },
-          );
-        },
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Dismissible Example'),
+        ),
+        body: ListView.builder(
+            itemCount: sweets.length,
+            itemBuilder: (context, index) {
+              return OpenContainer(
+                transitionDuration: const Duration(seconds: 1),
+                transitionType: ContainerTransitionType.fade,
+                openBuilder: (context, closeContainer) {
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text(sweets[index]),
+                    ),
+                    body: Center(
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            width: 200,
+                            height: 200,
+                            child: Icon(
+                              Icons.cake,
+                              size: 200,
+                              color: Colors.orange,
+                            ),
+                          ),
+                          Text(sweets[index])
+                        ],
+                      ),
+                    ),
+                  );
+                },
+                closedBuilder: (context, openContainer) {
+                  return Dismissible(
+                    key: Key(sweets[index]),
+                    child: ListTile(
+                      title: Text(sweets[index]),
+                      trailing: const Icon(Icons.cake),
+                      onTap: () {
+                        openContainer();
+                      },
+                    ),
+                    onDismissed: (direction) {
+                      sweets.removeAt(index);
+                    },
+                  );
+                },
+              );
+            }));
   }
 }
